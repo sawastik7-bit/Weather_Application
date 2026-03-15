c9f94dc5c83aba519467ab7bf85f7799e0bbbb16
Sawastik Bhullar <sawastik7@gmail.com>
2026-03-15 17:09:54 +0530

    added the vercel analytics package to track visitors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import {inject} from '@vercel/analytics'
 import App from './App.jsx'
-
+inject();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
